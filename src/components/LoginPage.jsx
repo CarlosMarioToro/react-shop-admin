e348a1c91700d65627968c8848f47b473daed7cf
CarlosMarioToro <111100510+CarlosMarioToro@users.noreply.github.com>
2023-08-08 20:17:46 -0500

    Clase 12

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,17 +1,21 @@
-import { LockClosedIcon } from '@heroicons/react/24/solid';
 import { useRef } from 'react';
+import { LockClosedIcon } from '@heroicons/react/24/solid';
+import { useAuth } from '@hooks/useAuth';
 
 export default function LoginPage() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
+  const auth = useAuth();
 
   const submitHandler = (event) => {
     event.preventDefault();
     const email = emailRef.current.value;
-    const passowrd = passwordRef.current.value;
+    const password = passwordRef.current.value;
 
-    console.log(email, passowrd);
-  }
+    auth.signIn(email, password).then(() => {
+      console.log('logged in');
+    });
+  };
 
   return (
     <>
