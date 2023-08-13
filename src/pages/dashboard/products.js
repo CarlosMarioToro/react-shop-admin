7e055ba40a85b6c41ca776233986354129dddcf3
CarlosMarioToro <111100510+CarlosMarioToro@users.noreply.github.com>
2023-08-13 09:38:00 -0500

    Clase 19

--- src/pages/dashboard/products.js
+++ src/pages/dashboard/products.js
@@ -1,15 +1,34 @@
-import { useState } from "react";
-import Pagination from "@components/pagination";
+import { useState } from 'react';
+import { CheckIcon } from '@heroicons/react/24/solid';
+import Modal from '@common/Modal';
 
-export default function products() {
+export default function Products() {
     const [products, setProducts] = useState([]);
+    const [open, setOpen] = useState(false);
     return (
         <>
+            <div className="lg:flex lg:items-center lg:justify-between mb-8">
+                <div className="flex-1 min-w-0">
+                    <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">List of Products</h2>
+                </div>
+                <div className="mt-5 flex lg:mt-0 lg:ml-4">
+                    <span className="sm:ml-3">
+                        <button
+                            type="button"
+                            className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            onClick={() => setOpen(true)}
+                        >
+                            <CheckIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+                            Add Product
+                        </button>
+                    </span>
+                </div>
+            </div>
             <div className="flex flex-col">
                 <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <Pagination limit={PRODUCT_LIMIT} offset={offset} total={totalProducts} handlePrev={handlePrev} handleNext={handleNext}></Pagination>
+                            {/* <Pagination limit={PRODUCT_LIMIT} offset={offset} total={totalProducts} handlePrev={handlePrev} handleNext={handleNext}></Pagination> */}
                             <table className="min-w-full divide-y divide-gray-200">
                                 <thead className="bg-gray-50">
                                     <tr>
@@ -67,11 +86,14 @@ export default function products() {
                                     ))}
                                 </tbody>
                             </table>
-                            <Pagination limit={PRODUCT_LIMIT} offset={offset} total={totalProducts} handlePrev={handlePrev} handleNext={handleNext}></Pagination>
+                            {/* <Pagination limit={PRODUCT_LIMIT} offset={offset} total={totalProducts} handlePrev={handlePrev} handleNext={handleNext}></Pagination> */}
                         </div>
                     </div>
                 </div>
             </div>
+            <Modal open={open} setOpen={setOpen}>
+                <h1>Hola Mundo</h1>
+            </Modal>
         </>
-    )
+    );
 }
